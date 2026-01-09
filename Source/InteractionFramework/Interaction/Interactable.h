99a80578849a171ce14d39a71d3919d779bc7cd7
emiirsahin <esahin.gh@gmail.com>
2026-01-09 12:41:05 +0300

    Add IInteractable interface

--- Source/InteractionFramework/Interaction/Interactable.h
+++ Source/InteractionFramework/Interaction/Interactable.h
@@ -3,6 +3,8 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Interface.h"
+#include "Interaction/InteractionTypes.h"
+#include "Interaction/InteractionDataAsset.h"
 #include "Interactable.generated.h"
 
 UINTERFACE(BlueprintType)
@@ -21,7 +23,7 @@ class UInteractable : public UInterface
  * Called by the InteractionComponent during focus detection and interaction execution.
  *
  * This interface does not handle input, detection, UI presentation, or interaction timing;
- * those concerns are managed externally by the interaction system.
+ * those concerns are managed externally by the interaction system, and mainly the interaction component.
  */
 
 class INTERACTIONFRAMEWORK_API IInteractable
@@ -29,4 +31,28 @@ class INTERACTIONFRAMEWORK_API IInteractable
 	GENERATED_BODY()
 
 public:
+	/** Returns the data describing how this interaction is presented in UI. */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Interaction")
+	UInteractionDataAsset* GetInteractionData() const;
+
+	/**
+	 * Returns current interaction state for UI presentation.
+	 * Must stay lightweight and safe.
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Interaction")
+	FInteractionQueryResult QueryInteraction(AActor* Interactor) const;
+
+	/**
+	 * Execute an interaction request, result depends on this object.
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Interaction")
+	void Interact(AActor* Interactor);
+
+	/** Cosmetic hook called when this object becomes the focused interaction target. */
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category="Interaction")
+	void OnFocusStart(AActor* Interactor);
+
+	/** Cosmetic hook called when this object is no longer the focused interaction target. */
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category="Interaction")
+	void OnFocusEnd(AActor* Interactor);
 };
